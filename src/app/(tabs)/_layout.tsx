27e55bea01fd5be87c9957d7b3c00d352a4bd2cf
Shawn Law <shawnlaw6669@gmail.com>
2025-05-17 14:45:08 +0800

    bottom tabs setup

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,57 @@
+import { View, Text } from 'react-native'
+import { AntDesign, Feather, Ionicons } from "@expo/vector-icons";
+import { Tabs } from 'expo-router';
+
+export default function _layout() {
+  return (
+    <Tabs
+      screenOptions={{ tabBarActiveTintColor: 'black' }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          headerTitle: 'Reddit',
+          title: 'Home',
+          headerTintColor: "#EB5528",
+          tabBarIcon: ({ color }) => <AntDesign name="home" size={24} color={color} />
+        }}
+      />
+      <Tabs.Screen
+        name="communities"
+        options={{
+          headerTitle: 'Communities',
+          title: 'Communities',
+          headerTintColor: "#EB5528",
+          tabBarIcon: ({ color }) => <Feather name="users" size={24} color={color} />
+        }}
+      />
+      <Tabs.Screen
+        name="create"
+        options={{
+          headerTitle: 'Create',
+          title: 'Create',
+          headerTintColor: "#EB5528",
+          tabBarIcon: ({ color }) => <AntDesign name="plus" size={24} color={color} />
+        }}
+      />
+      <Tabs.Screen
+        name="chat"
+        options={{
+          headerTitle: 'Chat',
+          title: 'Chat',
+          headerTintColor: "#EB5528",
+          tabBarIcon: ({ color }) => <Ionicons name="chatbubble-ellipses-outline" size={24} color={color} />
+        }}
+      />
+      <Tabs.Screen
+        name="inbox"
+        options={{
+          headerTitle: 'Inbox',
+          title: 'Inbox',
+          headerTintColor: "#EB5528",
+          tabBarIcon: ({ color }) => <Feather name="bell" size={24} color={color} />
+        }}
+      />
+    </Tabs>
+  )
+}
---
+++ src/app/(tabs)/communities.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Communities() {
+  return (
+    <View>
+      <Text>communities</Text>
+    </View>
+  )
+}
