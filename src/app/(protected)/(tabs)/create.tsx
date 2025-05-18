6b2bb66b546cfe0963670709c08a9307dcaffabc
Shawn Law <shawnlaw6669@gmail.com>
2025-05-18 14:02:38 +0800

    add the header x and post buttons

--- src/app/(protected)/(tabs)/create.tsx
+++ src/app/(protected)/(tabs)/create.tsx
@@ -1,10 +1,21 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import {Ionicons} from '@expo/vector-icons';
 
 export default function Create() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Create</Text>
+      <View style={styles.headerContainer}>
+        <TouchableOpacity onPress={() => console.log('close button pressed...')}>
+          <Ionicons name="close-outline" size={30} color="black" />
+        </TouchableOpacity>
+
+        <TouchableOpacity style={{marginLeft: 'auto'}} onPress={() => console.log('pressed...')}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Post</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   )
 }
@@ -13,5 +24,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white'
-  }
+  },
+  headerContainer: {
+    flexDirection: 'row',
+    marginHorizontal: 10
+  },
+    button: {
+    backgroundColor: '#1E4396',
+    paddingVertical: 3,
+    paddingHorizontal: 6,
+    borderRadius: 20
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold'
+  },
 })
