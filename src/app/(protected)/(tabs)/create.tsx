9c6fb3f753d985c5e26458fb75b6f114d4585bfd
Shawn Law <shawnlaw6669@gmail.com>
2025-05-18 14:22:06 +0800

    subreddit selector button

--- src/app/(protected)/(tabs)/create.tsx
+++ src/app/(protected)/(tabs)/create.tsx
@@ -1,12 +1,14 @@
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import {Ionicons} from '@expo/vector-icons';
+import { router } from 'expo-router';
 
 export default function Create() {
   return (
     <SafeAreaView style={styles.container}>
+      {/* HEADER */}
       <View style={styles.headerContainer}>
-        <TouchableOpacity onPress={() => console.log('close button pressed...')}>
+        <TouchableOpacity onPress={() => router.back()}>
           <Ionicons name="close-outline" size={30} color="black" />
         </TouchableOpacity>
 
@@ -15,6 +17,13 @@ export default function Create() {
             <Text style={styles.buttonText}>Post</Text>
           </View>
         </TouchableOpacity>
+      </View>
+      {/* END HEADER */}
+
+      {/* SUBREDDIT SELECTOR */}
+      <View style={styles.subredditPicker}>
+        <Text style={styles.rText}>r/</Text>
+        <Text style={styles.selectCommunityText}>Select a community</Text>
       </View>
     </SafeAreaView>
   )
@@ -27,16 +36,36 @@ const styles = StyleSheet.create({
   },
   headerContainer: {
     flexDirection: 'row',
-    marginHorizontal: 10
+    marginHorizontal: 10,
+    alignItems: 'center'
   },
     button: {
     backgroundColor: '#1E4396',
-    paddingVertical: 3,
-    paddingHorizontal: 6,
+    paddingVertical: 6,
+    paddingHorizontal: 9,
     borderRadius: 20
   },
   buttonText: {
     color: 'white',
     fontWeight: 'bold'
+  },
+  subredditPicker: {
+    flexDirection: 'row',
+    backgroundColor: '#EDEDED',
+    padding: 10,
+    borderRadius: 20,
+    gap: 7,
+    alignSelf: 'flex-start',
+    marginVertical: 10,
+  },
+  rText: {
+    backgroundColor: 'black',
+    color: 'white',
+    paddingHorizontal: 5,
+    borderRadius: 10,
+    fontWeight: 'bold'
+  },
+  selectCommunityText: {
+    fontWeight: '500'
   },
 })
