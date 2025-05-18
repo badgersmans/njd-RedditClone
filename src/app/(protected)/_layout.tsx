095876909e3fd9a467ae450fc91020c2c4507a1a
Shawn Law <shawnlaw6669@gmail.com>
2025-05-18 13:25:02 +0800

    beautiful headers!

--- src/app/(protected)/_layout.tsx
+++ src/app/(protected)/_layout.tsx
@@ -1,5 +1,8 @@
-import { Redirect, Stack } from 'expo-router'
+import { Redirect, router, Stack } from 'expo-router'
 import { useAuth } from '@clerk/clerk-expo'
+import {Entypo, AntDesign, Ionicons} from '@expo/vector-icons';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
 
 export default function ProtectedRoutesRootLayout() {
   const { isSignedIn } = useAuth()
@@ -11,7 +14,52 @@ export default function ProtectedRoutesRootLayout() {
   return (
     <Stack>
       <Stack.Screen name='(tabs)' options={{headerShown: false}}/>
+      <Stack.Screen 
+        name='post/[id]' 
+        options={{
+          headerTitle: '',
+          headerStyle: {backgroundColor: '#EB5528'},
+          animation: 'slide_from_bottom',
+          headerLeft: () => (
+            <TouchableOpacity style={styles.circularButton} onPress={() => router.back()}>
+              <Ionicons name="arrow-back-outline" size={23} color="white" />
+            </TouchableOpacity>
+          ),
+          headerRight: () => (
+            <View style={styles.headerRightContainer}>
+              <View style={styles.circularButton}>
+                <AntDesign name="search1" size={21} color="white" />
+              </View>
+
+              <View style={styles.circularButton}>
+                <MaterialCommunityIcons name="share-outline" size={24} color="white" />
+              </View>
+
+              <View style={styles.circularButton}>
+                <Entypo name="dots-three-horizontal" size={18} color="white" />
+              </View>
+            </View>
+          )
+        }}
+      />
     </Stack>
-      // <Stack screenOptions={{headerShown: false}}/>
   )
 }
+
+const styles = StyleSheet.create({
+  circularButton: {
+    backgroundColor: '#1A0E21',
+    width: 36,
+    height: 36,
+    borderRadius: 18, // half of width/height for perfect circle
+    alignItems: 'center',
+    justifyContent: 'center',
+    opacity: 0.8,
+    // marginHorizontal: 4, // spacing between buttons
+  },
+  headerRightContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 10
+  }
+})
